ebb9672e99e9e2a5774d264c5d99caa29ab52b6d
piverlc <piverlc.bw@gmail.com>
2022-10-14 20:07:13 +0530

    removed useExpenseTitle hook

--- src/hooks/useExpenseTitle.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-export const useExpenseTitle = () => {
-  const [title, setTitle] = useState("Expense Tracker");
-  useEffect(() => {
-    setTitle((document.title = title));
-  }, [title]);
-  return { title } as const;
-};
